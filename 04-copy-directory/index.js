e3519bef90746539846299558dad1fc678018ab3
sorvigolovaa <sbknugaara@gmail.com>
2023-05-05 21:16:50 +0500

    feat: fourth task complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyDir(sourceDir, targetDir) {
+  // Создаем папку files-copy, если она не существует
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+
+  // Читаем содержимое папки files
+  const files = fs.readdirSync(sourceDir);
+
+  // Копируем файлы из папки files в папку files-copy
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const targetPath = path.join(targetDir, file);
+
+    if (fs.statSync(sourcePath).isFile()) {
+      fs.copyFileSync(sourcePath, targetPath);
+    }
+  });
+
+  console.log('Папка успешно скопирована.');
+}
+
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+copyDir(sourceDir, targetDir);
 
